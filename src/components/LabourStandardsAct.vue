b24ab75d157814fa77ca2be3ca829841e1e5a5ed
BobbyHo <angela.bobby@gmail.com>
2018-07-18 19:09:03 +0800

    [Improve] Move articleMenu from right to left

--- src/components/LabourStandardsAct.vue
+++ src/components/LabourStandardsAct.vue
@@ -3,13 +3,8 @@
 		<h2 is="sui-header">勞ㄐ法</h2>
 		<sui-grid>
 			<sui-grid-row>
-				<sui-grid-column width="twelve">
-					<section v-for="articleItem in actData" class="article" :id="`section-${articleItem.article}`">
-						<h3 is="sui-header">第 {{articleItem.article}} 條</h3>
-						<p>{{articleItem.content}}</p>
-					</section>
-				</sui-grid-column>
 				<sui-grid-column width="four">
+					<h3 is="sui-header">目錄</h3>
 					<div id="articleMenu">
 						<sui-menu secondary vertical fluid>
     						<sui-menu-item link v-for="articleItem in actData" v-scroll-to="`#section-${articleItem.article}`">
@@ -18,6 +13,12 @@
 							</sui-menu-item>
 						</sui-menu>
 					</div>
+				</sui-grid-column>
+				<sui-grid-column width="twelve">
+					<section v-for="articleItem in actData" class="article" :id="`section-${articleItem.article}`">
+						<h3 is="sui-header">第 {{articleItem.article}} 條</h3>
+						<p>{{articleItem.content}}</p>
+					</section>
 				</sui-grid-column>
 			</sui-grid-row>
 		</sui-grid>
