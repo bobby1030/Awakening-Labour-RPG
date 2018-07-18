7286f4edc43bba561b2e4f83f7cc9ac6cffc0b8f
BobbyHo <angela.bobby@gmail.com>
2018-07-18 22:18:07 +0800

    [Improve] Fix duplicated entries in CompanyRule when role is GM

--- src/components/CompanyRule.vue
+++ src/components/CompanyRule.vue
@@ -17,7 +17,6 @@
 						'若週營業未滿五天，總公司將前來視察並視情況懲罰。',
 						'若有員工意圖發起罷工且成功，該日收益歸零；若失敗（投票未過），公司收益-25%。',
 						'每位員工每日值班時數不得超過 15 小時，每週總值班時數不得超過 56 小時，若違反，總公司將前來視察並強迫調整班表。',
-						'總公司體諒剌剌族同仁多有扶養家庭之經濟壓力，故提供多元的加薪及升遷機會給所有剌剌族族人，鼓勵各位多多爭取機會。'
 					],
 					employee: [
 						'資方每週排定班表，員工可單方面提出調整意見，但資方無進行討論或接受要求之義務。',
@@ -26,6 +25,8 @@
 						'若員工表現不稱職，資方可進行解雇。',
 						'事業單位違反勞動基準法，經申訴後，勞檢機構得處以罰鍰或強迫事業單位為全體員工加薪。',
 						'其餘勞動相關規範，請參見〈勞動ㄐ準法〉。',
+					],
+					common: [
 						'總公司體諒剌剌族同仁多有扶養家庭之經濟壓力，故提供多元的加薪及升遷機會給所有剌剌族族人，鼓勵各位多多爭取機會。'
 					]
 				}
@@ -42,11 +43,11 @@
 			},
 			getRules(role) {
 				if (this.getRoleGroup(role) === 'employee') {
-					return this.rules.employee;
+					return this.rules.employee.concat(this.rules.common);
 				} else if (this.getRoleGroup(role) === 'employer') {
-					return this.rules.employer;
+					return this.rules.employer.concat(this.rules.common);
 				} else if (this.getRoleGroup(role) === 'GM') {
-					return this.rules.employee.concat(this.rules.employer)
+					return this.rules.employee.concat(this.rules.employer, this.rules.common);
 				}
 			}
 		}
