9f0afeef306f68c14a8afb3bc1f04c38856ebf18
BobbyHo <angela.bobby@gmail.com>
2018-07-25 00:34:38 +0800

    [Improve] Highlight the target of selected menu item

--- src/components/LabourStandardsAct.vue
+++ src/components/LabourStandardsAct.vue
@@ -7,7 +7,7 @@
 					<h3 is="sui-header">目錄</h3>
 					<div id="articleMenu">
 						<sui-menu secondary vertical fluid>
-    						<sui-menu-item link v-for="articleItem in actData" v-scroll-to="`#section-${articleItem.article}`">
+    						<sui-menu-item link v-for="articleItem in actData" v-scroll-to="`#section-${articleItem.article}`" @click="onArticleMenuClick(articleItem.article)">
 								&sect; {{articleItem.article}}
 								<div class="itemDesc">{{articleItem.content}}</div>
 							</sui-menu-item>
@@ -15,7 +15,7 @@
 					</div>
 				</sui-grid-column>
 				<sui-grid-column width="twelve">
-					<section v-for="articleItem in actData" class="article" :id="`section-${articleItem.article}`">
+					<section v-for="articleItem in actData" :class="{article: true, active: isActiveArticle(articleItem.article)}" :id="`section-${articleItem.article}`">
 						<h3 is="sui-header">第 {{articleItem.article}} 條</h3>
 						<p>{{articleItem.content}}</p>
 					</section>
@@ -34,16 +34,59 @@ Vue.use(VueScrollTo, {
 });
 
 export default {
+	data() {
+		return {
+			activeArticle: null,
+		}
+	},
 	computed: {
 		actData() {
 			return actData;
+		}
+	},
+	methods: {
+		isActiveArticle(artNum) {
+			if (this.activeArticle === artNum) {
+				return true;
+			} else {
+				return false;	
+			}
+		},
+		onArticleMenuClick(artNum) {
+			this.activeArticle = null;
+			setTimeout(()=>{this.activeArticle = artNum;}, 100);
+			
 		}
 	}
 }
 </script>
 <style scoped>
+@keyframes blink {
+	0% {
+		background-color: transparent
+	}
+	20% {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	80% {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	100% {
+		background-color: transparent
+	}
+}
+
 section.article {
 	margin-bottom: 2em;
+	padding: 0.8em 1em;
+	border-radius: 5px;
+}
+
+section.article.active {
+	animation-name: blink;
+	animation-iteration-count: 1;
+	animation-timing-function: cubic-bezier(0,.41,1,.59);
+	animation-duration: 0.6s;
 }
 
 section.article > p {
